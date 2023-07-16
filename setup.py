7a3fc4a260334c9575ad899611caab9454bca6d9
Kaiaicy <zykycy@gmail.com>
2023-07-16 21:41:28 +0800

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zhijian',
-    version='1.0.0',
+    version='0.0.1',
     description='ZhiJian: A Unifying and Rapidly Deployable Toolbox for Pre-trained Model Reuse',
     author='ZhiJian Contributors',
     author_email='',
--- zhijian/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from zhijian import data, models, trainers
-
-__version__ = "0.0.1"
-
-__all__ = [
-    "data",
-    "models",
-    "trainers"
-]
